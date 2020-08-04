2f51f74e67d52c3aa0bfb120538c71b4ff67831e
David Revelo <david@aperturaradical.org>
2020-08-04 07:36:57 -0500

    Version 2 (bind syntax)

--- half.rb
+++ half.rb
@@ -1,17 +1,21 @@
+require 'dry/monads'
+
+include Dry::Monads[:maybe]
+
 def half(number)
-  return if number.odd?
-
-  number / 2
-end
-
-result = half 20
-
-if !result.nil?
-  result = half result
-
-  if !result.nil?
-    result = half result
+  if number.even?
+    Some(number / 2)
+  else
+    None()
   end
 end
 
-p result
+def result(number)
+  half(number).bind do |n|
+    half(n).bind do |n|
+      half(n)
+    end
+  end
+end
+
+p result(20)
